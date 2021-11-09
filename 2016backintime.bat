c8b4131ec7e71dbe45a45796a1b48eac864cb9a9
Theday2351 <87524208+Theday2351@users.noreply.github.com>
2021-11-09 21:31:46 +0300

    Add files via upload

---
+++ 2016backintime.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ur back in 2016
+date 1.1.2016
+pause
+clear
+echo bye
+exit
+ ( ELSE (
+@echo off
+clear
+YOU RUNNED IT IN NO ADMIN PRIVELGE IDIOT
+GO AND RUN ON ADMIN
+pause
+clear
+exit
